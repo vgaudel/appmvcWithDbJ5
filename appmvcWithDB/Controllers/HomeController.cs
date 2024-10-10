7dac4c5c90b9a482789ded95135781998aa56183
vgaudel <vgaudel@gmail.com>
2024-10-10 09:45:20 +0200

    début correction

---
+++ appmvcWithDB/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using appmvcWithDB.Models;
+using appmvcWithDB.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace appmvcWithDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Sejour sejour = new Sejour { Lieu = "Chambord", Telephone = "11111111" };
+
+            HomeViewModel hvm = new HomeViewModel
+            {
+                Message = "Bonjour tout le monde",
+                Date = DateTime.Now,
+                Sejour = sejour
+            };
+
+            return View(hvm);
+        }
+    }
+}
---
+++ appmvcWithDB/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using appmvcWithDB.Models;
+using System;
+
+namespace appmvcWithDB.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public Sejour Sejour { get; set; }
+    }
+}
